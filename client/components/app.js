de147b68f37e2716b15c14b7939733c3bfafbb93
Leong Wai Yin <waiyin.leong@experian.com>
2017-09-12 15:56:34 +0800

    Move all tests to sandbox folder to keep things separate from boilerplate code

--- client/components/app.js
+++ client/components/app.js
@@ -1,35 +1,13 @@
 import React from 'react';
-import TestState from './test-state';
-import TestFunction from './test-function';
+import Sandbox from './sandbox/sandbox';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      addLabel: "add me",
-      minusLabel: "minus me"
-    }
-  }
-
-  handleUpdateProps() {
-    this.setState({
-      addLabel: "ADD " + new Date().getMilliseconds(),
-      minusLabel: "MINUS " + new Date().getMilliseconds(),
-    })
-  }
-
-  handleButton() {
-    console.log("custom function (handleButton) called.");
-  }
-
   render() {
     return (
       <div style={{textAlign: 'center'}}>
         <h1>Hello World</h1>
-        <button onClick={this.handleUpdateProps.bind(this)}>Update Label Prop</button>
-        <TestState addLabel={this.state.addLabel} minusLabel={this.state.minusLabel} />
-        <TestFunction customHandler={this.handleButton.bind(this)} />
+        <hr />
+        <Sandbox />
       </div>
     );
   }
