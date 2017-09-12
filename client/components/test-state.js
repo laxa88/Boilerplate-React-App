fb31f2b003e07ebc70648fe5162f1ece16ff5492
Leong Wai Yin <waiyin.leong@experian.com>
2017-09-12 15:46:59 +0800

    update test-state

--- client/components/test-state.js
+++ client/components/test-state.js
@@ -7,60 +7,28 @@ export default class TestState extends React.Component {
     this.state = {
       counter: 0
     }
-
-    console.log("constructor");
   }
 
   // Custom functions
 
   handleAdd() {
-    console.log("handleAdd");
     this.setState({ counter: this.state.counter += 1 })
   }
 
   handleMinus() {
-    console.log("handleMinus");
     this.setState({ counter: this.state.counter -= 1 })
   }
 
   // Lifecycle functions
 
-  componentWillMount() {
-    console.log("componentWillMount");
-  }
-
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log("componentWillReceiveProps", nextProps);
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("shouldComponentUpdate", nextProps, nextState);
-    return true;
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log("componentWillUpdate", nextProps, nextState);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate", prevProps, prevState);
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-  }
-
   render() {
-    console.log("render");
     return (
       <div>
         <div>{this.state.counter}</div>
-        <button id="add-button" onClick={this.handleAdd.bind(this)}>{this.props.addLabel}</button>&nbsp;
-        <button id="minus-button" onClick={this.handleMinus.bind(this)}>{this.props.minusLabel}</button>
+        <p>
+          <button id="add-button" onClick={this.handleAdd.bind(this)}>{this.props.addLabel}</button>&nbsp;
+          <button id="minus-button" onClick={this.handleMinus.bind(this)}>{this.props.minusLabel}</button>
+        </p>
       </div>
     );
   }
