8cb933d1d1f03826406daf565e9045c4f6769028
Leong Wai Yin <waiyin.leong@experian.com>
2017-09-12 16:39:02 +0800

    Update index.js

--- client/index.js
+++ client/index.js
@@ -10,10 +10,12 @@ import ReactDOM from 'react-dom';
 
 // Use this block to test React with Redux
 import { Provider } from "react-redux";
-import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
 import ReduxApp from './redux/redux-app';
 import testReducer from './redux/reducer';
-let store = createStore(testReducer);
+const enhancer = composeWithDevTools();
+const store = createStore(testReducer, enhancer);
 ReactDOM.render(
   <Provider store={store}>
     <ReduxApp />
