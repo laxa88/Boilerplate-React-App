58a2c1e8229de88622e6b939bc851f4f4d05bf5d
Leong Wai Yin <waiyin.leong@experian.com>
2017-09-13 15:15:35 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
+// https://eslint.org/docs/user-guide/configuring
+// 0 = "off", 1 = "warn", 2 = "error"
 module.exports = {
-    "extends": "airbnb"
+  "extends": [
+    "airbnb", // import airbnb's eslint rules
+  ],
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], // allows .js files to have .jsx syntax
+    "import/no-extraneous-dependencies": [1], // ignore error for "config" import alias
+    "import/no-unresolved": [1], // ignore error for "config" import alias
+    "import/extensions": [1], // ignore error for "config" import alias
+    "linebreak-style": [2, "windows"], // Ignores LF (Unix) endlines in favour for CRLF
+    "max-len": [1, 100, 2, {ignoreComments: true}], // Only warn for lines exceeding max-len
+    "quotes": [2, "double"],
+  }
 };
