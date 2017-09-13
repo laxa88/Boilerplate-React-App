acb3163f2af82197256646e4960fd4c90fd2c1a0
Leong Wai Yin <waiyin.leong@experian.com>
2017-09-13 15:24:53 +0800

    Add jest plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
   "extends": [
     "airbnb", // import airbnb's eslint rules
   ],
+  "plugins": [
+    "jest", // required by "env" to allow jest tests to use global variables
+  ],
   "rules": {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], // allows .js files to have .jsx syntax
     "import/no-extraneous-dependencies": [1], // ignore error for "config" import alias
@@ -12,5 +15,9 @@ module.exports = {
     "linebreak-style": [2, "windows"], // Ignores LF (Unix) endlines in favour for CRLF
     "max-len": [1, 100, 2, {ignoreComments: true}], // Only warn for lines exceeding max-len
     "quotes": [2, "double"],
+  },
+  "env": {
+    "browser": true, // allows the use browser variables, e.g. "document.xxx"
+    "jest/globals": true,
   }
 };
