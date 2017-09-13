d3f40fe67d5c737d4fbcd55e26521b9676dccabe
Leong Wai Yin <waiyin.leong@experian.com>
2017-09-13 15:41:20 +0800

    Lint sandbox.js

--- client/components/sandbox/sandbox.js
+++ client/components/sandbox/sandbox.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import TestState from './test-state';
-import TestFunction from './test-function';
+import React from "react";
+import TestState from "./test-state";
+import TestFunction from "./test-function";
 
 export default class Sandbox extends React.Component {
   constructor(props) {
@@ -8,24 +8,25 @@ export default class Sandbox extends React.Component {
 
     this.state = {
       addLabel: "add me",
-      minusLabel: "minus me"
-    }
+      minusLabel: "minus me",
+    };
   }
 
   handleUpdateProps() {
     this.setState({
-      addLabel: "ADD " + new Date().getMilliseconds(),
-      minusLabel: "MINUS " + new Date().getMilliseconds(),
-    })
+      addLabel: `ADD ${new Date().getMilliseconds()}`,
+      minusLabel: `MINUS ${new Date().getMilliseconds()}`,
+    });
   }
 
   handleButton() {
     console.log("custom function (handleButton) called.");
+    this.forceUpdate();
   }
 
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
+      <div style={{ textAlign: "center" }}>
         <button onClick={this.handleUpdateProps.bind(this)}>Update Label Prop</button>
         <TestState addLabel={this.state.addLabel} minusLabel={this.state.minusLabel} />
         <TestFunction customHandler={this.handleButton.bind(this)} />
