ac40077e138ada190ebaa5c1130541b50fda3230
Leong Wai Yin <waiyin.leong@experian.com>
2017-09-13 18:23:14 +0800

    Fix failing test on mounting component with redux, thunk and fetch

--- client/components/redux-sandbox/test-function-redux.test.js
+++ client/components/redux-sandbox/test-function-redux.test.js
@@ -2,25 +2,39 @@ import React from "react";
 import { Provider } from "react-redux";
 import * as enzyme from "enzyme";
 import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
 import TestFunctionRedux from "./test-function-redux";
 
-const mockStore = configureStore();
-const dispatch = jest.fn();
+// Mock fetch() used by TestFunctionRedux > dispatch > loadSomeData
+global.fetch = require("jest-fetch-mock");
+
+// Mock redux store with thunk (adds asynchronous dispatching)
+const mockStore = configureStore([thunk]);
 
 describe("test-function-redux.js", () => {
   it("Should shallow render with mocked redux store", () => {
+    const store = mockStore();
+
     const wrapper = enzyme.shallow(
-      <Provider store={mockStore({ dispatch })}>
+      <Provider store={store}>
         <TestFunctionRedux customHandler={() => { console.log("WOAH"); }} />
       </Provider>,
     );
+
     expect(wrapper).toMatchSnapshot();
   });
 
-  // it("Should mount", () => {
-  //   const wrapper = enzyme.mount(<TestFunctionRedux customHandler={() => { console.log("WOAH"); }} />);
-  //   expect(wrapper).toBeDefined();
-  // });
+  it("Should mount with mocked redux store and thunk", () => {
+    const store = mockStore({});
+
+    const wrapper = enzyme.mount(
+      <Provider store={store}>
+        <TestFunctionRedux customHandler={() => { console.log("WOAH"); }} />
+      </Provider>,
+    );
+
+    expect(wrapper).toBeDefined();
+  });
 
   // it("Should call the custom callback function that was passed in as prop", () => {
   //   const spy = jest.fn();
